edf42cd61edcf7fe7ab503d81167bde3a9ca494b
ramin <ramoures@gmail.com>
2025-02-19 15:53:54 +0330

    comment edited.

--- bin/minifix.js
+++ bin/minifix.js
@@ -25,7 +25,7 @@ if (!existsSync(configPath)) {
  */`;
     writeFileSync(configPath, defaultConfig);
     console.log(`${textGreen('Configuration file created: minifix.config.js')}`);
-    console.log(`Now you can customize your settings in minifix.config.js and run the ${textBlue('minifix')} again.\n`);
+    console.log(`Now you can customize your settings in ${textYellow('minifix.config.js')} and run the ${textBlue('npx minifix')} again.\n`);
     process.exit(0);
 }
 
